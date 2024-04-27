7a2c9f822056a0fbf96aa7d7fc9aa2ef4738f329
NAVEEN S <naveensiva72@gmail.com>
2024-04-27 23:35:14 +0530

    Time: 21 ms (96.90%) | Memory: 46.1 MB (78.22%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,18 +1,34 @@
+import java.util.HashMap;
+
 class Solution {
-    public int subarraySum(int[] nums, int k) {
-        int ans=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            int sum=0;
-            for(int j=i;j<nums.length;j++)
-            {
-                sum+=nums[j];
-                if(sum==k)
-                {
-                    ans++;
-                }
-            }
+    public int subarraySum(int[] arr, int k) {
+        int n = arr.length; // take the size of the array
+        
+        int[] prefix = new int[n]; // make a prefix array to store prefix sum
+        
+        prefix[0] = arr[0]; // for element at index at zero, it is same
+        
+        // making our prefix array
+        for(int i = 1; i < n; i++) {
+            prefix[i] = arr[i] + prefix[i - 1];
         }
-        return ans;
+        
+        HashMap<Integer, Integer> mp = new HashMap<>(); // declare a HashMap
+        
+        int ans = 0; // to store the number of our subarrays having sum as 'k'
+        
+        for(int i = 0; i < n; i++) { // traverse from the prefix array
+            if(prefix[i] == k) // if it already becomes equal to k, then increment ans
+                ans++;
+            
+            // now, as we discussed find whether (prefix[i] - k) present in map or not
+            if(mp.containsKey(prefix[i] - k)) {
+                ans += mp.get(prefix[i] - k); // if yes, then add it our answer
+            }
+            
+            mp.put(prefix[i], mp.getOrDefault(prefix[i], 0) + 1); // put prefix sum into our map
+        }
+        
+        return ans; // and at last, return our answer
     }
 }
