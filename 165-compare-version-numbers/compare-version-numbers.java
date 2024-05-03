47e32530930a9ec935582d3d13c336e305906233
NAVEEN S <naveensiva72@gmail.com>
2024-05-03 15:29:08 +0530

    Time: 15 ms (5.41%) | Memory: 41.9 MB (5.03%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+        int i = 0;
+        System.out.println(Arrays.toString(v1) + "  " + Arrays.toString(v2));
+        while (i < v1.length && i < v2.length) {
+            long a = convertToInt(v1[i]);
+            long b = convertToInt(v2[i]);
+            System.out.println(a + "  " + b);
+            if (a < b) {
+                return -1;
+            } else if (a > b) {
+                return 1;
+            }
+            i++;
+        }
+        if (i == v1.length && i < v2.length) {
+            for (; i < v2.length; i++) {
+                if (convertToInt(v2[i]) != 0) {
+                    return -1;
+                }
+            }
+        } else if (i == v2.length && i < v1.length) {
+            for (; i < v1.length; i++) {
+                if (convertToInt(v1[i]) != 0) {
+                    return 1;
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    public long convertToInt(String version) {
+        long ans = 0;
+        for (int i = 0; i < version.length(); i++) {
+            ans = (ans * 10) + Integer.parseInt(version.charAt(i) + "");
+        }
+        return ans;
+    }
+}
