09b49e5b01f3a9d0ab81f635d2376721113a49cf
NAVEEN S <naveensiva72@gmail.com>
2024-05-06 00:18:45 +0530

    Time: 2 ms (85.96%) | Memory: 46.3 MB (18.42%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,17 +1,18 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int changePosition=0;
-        for(int i=0;i<nums.length;i++)
-        {
+        int changePosition = 0;
+        for (int i = 0; i < nums.length;i++) {
             if(nums[i]!=0)
             {
                 nums[changePosition]=nums[i];
-                if(i!=changePosition)
+                if(changePosition!=i)
                 {
                     nums[i]=0;
                 }
                 changePosition++;
+                
             }
         }
+        return;
     }
 }
