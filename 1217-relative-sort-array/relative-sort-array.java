e0c18d80b57c5e0e95aa279fbe8200658bac9616
NAVEEN S <naveensiva72@gmail.com>
2024-06-12 00:48:29 +0530

    Time: 41 ms (5.74%) | Memory: 45.4 MB (6.92%) - LeetSync

---
+++ 1217-relative-sort-array/relative-sort-array.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int count=0;
+        int i=0;
+        for(i=0;i<arr1.length && count<arr2.length;)
+        {
+            int j=i;
+            while(j<arr1.length)
+            {
+                if(arr1[j]==arr2[count])
+                {
+                    int temp=arr1[i];
+                    arr1[i++]=arr1[j];
+                    arr1[j]=temp;
+                }
+                j++;
+            }
+            count++;
+        }
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(i +"  "+count);
+        if(i<arr1.length)
+        {
+            cyclicSort(arr1 , i);
+        }
+
+        return arr1;
+    }
+    public static void cyclicSort(int[] arr1 , int st)
+    {
+        for(int i=st;i<arr1.length;i++)
+        {
+            int j=i+1;
+            System.out.println(j +"  "+i+"  "+Arrays.toString(arr1));
+            while((j>st && j<arr1.length) && arr1[j - 1]>arr1[j])
+            {
+                int temp=arr1[j];
+                arr1[j]=arr1[j-1];
+                arr1[j-1]=temp;
+                j--;
+            }
+        }
+    }
+}
