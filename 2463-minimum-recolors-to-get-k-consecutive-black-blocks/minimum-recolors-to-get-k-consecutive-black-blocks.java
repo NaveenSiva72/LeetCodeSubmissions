6932284e1d79b220671ee665d708a2972f317fa5
NAVEEN S <naveensiva72@gmail.com>
2025-03-08 22:58:23 +0530

    Time: 4 ms (10.90%) | Memory: 41.5 MB (74.80%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -6,40 +6,18 @@ class Solution {
         {
             int curr = i;
             int count = 1;
-            System.out.println("initial"+ action);
             while(curr < blocks.length() && count++ <= k )
             {
                 if(blocks.charAt(curr) == 'W'){
                     action++;
                 }
                 curr++;
-                System.out.println("inner"+action);
             }
-            System.out.println(action);
             if(action < ans){
                 ans = action;
             }
             action = 0;
         }
-
-        // for(int i=blocks.length() - 1;((i+1)-k)>=0;i--)
-        // {
-        //     int curr = i;
-        //     // System.out.println(i);
-        //     // System.out.println(curr);
-        //     while(curr >= 0)
-        //     {
-        //         if(blocks.charAt(curr) == 'W'){
-        //             action++;
-        //         }
-        //         curr--;
-        //     }
-        //     if(action < ans){
-        //         ans = action;
-        //     }
-        //     System.out.println(action);
-        //     action = 0;
-        // }
         return ans;
     }
 }
