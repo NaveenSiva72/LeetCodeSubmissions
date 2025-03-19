69d76d0d09ea765fedf36457a75a786384346f08
NAVEEN S <naveensiva72@gmail.com>
2025-03-20 00:46:23 +0530

    Time: 15 ms (10.74%) | Memory: 45.3 MB (15.31%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -1,35 +1,46 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        Queue<Character> arr = new LinkedList<>();
+        // Queue<Character> arr = new LinkedList<>();
 
-        int op= 0;
-        String ans = "";
-        for(int i=0;i<s.length();i++)
-        {
-            if(s.charAt(i)==')'){
-                op--;
-                if(op==0){
-                    ans = ans + putIntoQueue(arr);
-                }else{
-                    arr.add(')');
-                }
-            }else{
-                op++;
-                 arr.add('(');
+        // int op= 0;
+        // String ans = "";
+        // for(int i=0;i<s.length();i++)
+        // {
+        //     if(s.charAt(i)==')'){
+        //         op--;
+        //         if(op==0){
+        //             ans = ans + putIntoQueue(arr);
+        //         }else{
+        //             arr.add(')');
+        //         }
+        //     }else{
+        //         op++;
+        //          arr.add('(');
                 
+        //     }
+        // }
+        // return ans;    
+        int flag=0;
+        String ans ="";
+        for(int i=0;i<s.length();i++){
+            char curr = s.charAt(i); 
+            if(!(flag==0 && s.charAt(i)=='(') && !(flag==1 && s.charAt(i)==')')){
+                ans = ans + (curr+"");
             }
-        }
-        return ans;    
-    }
-    
-    public String putIntoQueue(Queue<Character> arr)
-    {
-        String ans = "";
-        arr.poll();
-        while (!arr.isEmpty()) { 
-            char i = arr.poll();
-            ans += i; 
+            if (curr == '(') flag++;
+            else flag--;
         }
         return ans;
     }
+    
+    // public String putIntoQueue(Queue<Character> arr)
+    // {
+    //     String ans = "";
+    //     arr.poll();
+    //     while (!arr.isEmpty()) { 
+    //         char i = arr.poll();
+    //         ans += i; 
+    //     }
+    //     return ans;
+    // }
 }
