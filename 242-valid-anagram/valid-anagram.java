0fca95b11a9eaf1ff98e8419a466804f615f1c58
NAVEEN S <naveensiva72@gmail.com>
2025-03-29 23:01:34 +0530

    Time: 4 ms (75.28%) | Memory: 43.1 MB (84.48%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,24 +1,24 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if((s.length()) != t.length())
-        {
-            return false;
-        }
-        HashMap<Character , Integer> sCharactedAndCount=new HashMap<>();
-        HashMap<Character , Integer> tCharactedAndCount=new HashMap<>();
+        int[] count = new int[26];
+        if(s.length() != t.length()) return false;
 
-        for(int i=0;i<s.length();i++)
-        {
-            sCharactedAndCount.put(s.charAt(i) , sCharactedAndCount.getOrDefault(s.charAt(i) , 0)+1);
-            tCharactedAndCount.put(t.charAt(i) , tCharactedAndCount.getOrDefault(t.charAt(i) , 0)+1);
+        for (int i=0;i<s.length();i++) {
+            count[s.charAt(i) - 'a']++;
         }
-        for(char i : sCharactedAndCount.keySet())
-        {
-            if(tCharactedAndCount.getOrDefault(i , 0) < sCharactedAndCount.getOrDefault(i , 0 ))
-            {
+        
+        for (int i=0;i<s.length();i++){
+            count[t.charAt(i) - 'a']--;
+        }
+        
+        for (int val : count) {
+            if (val != 0) {
                 return false;
             }
         }
+
+
         return true;
+
     }
 }
