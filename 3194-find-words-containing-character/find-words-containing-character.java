a097aa8d0c8d393c52ad9b21393228dd660c6cce
NAVEEN S <naveensiva72@gmail.com>
2025-05-24 14:41:16 +0530

    Time: 2 ms (52.59%) | Memory: 44.9 MB (76.79%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.java
+++ 3194-find-words-containing-character/find-words-containing-character.java
@@ -1,13 +1,15 @@
 class Solution {
     public List<Integer> findWordsContaining(String[] words, char x) {
-        List<Integer> ans = new ArrayList<>();
-        for(int i=0;i<words.length;i++)
-        {
-            if(words[i].contains(x+""))
-            {
-                ans.add(i);
+        List<Integer> arr = new ArrayList<>();
+        int count;
+        String xx = x+"";
+        for(int i=0;i<words.length;i++){
+            if(words[i].contains(xx)){
+                arr.add(i);
             }
+
         }
-        return ans;
+        return arr;
+        
     }
 }
