b5335b6f97626afa249dd357dd3cc86d9224a3f8
NAVEEN S <naveensiva72@gmail.com>
2025-08-20 00:03:42 +0530

    Time: 2 ms (17.95%) | Memory: 45.7 MB (46.36%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,40 +1,30 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        if (matrix.length == 0){return ;}
 
-        int rowLen = matrix.length;
-        int colLen = matrix[0].length;
+        HashSet<Integer> row = new HashSet<>();
+        HashSet<Integer> col = new HashSet<>();
 
-        int[] row = new int[rowLen];
-        int[] col = new int[colLen];
-
-        for(int i=0;i<rowLen;i++){
-            for(int j=0;j<colLen;j++){
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
                 if(matrix[i][j]==0){
-                    row[i]=1;
-                    col[j]=1;
-                }
-            }
-        }
-        setMatrixToZero(matrix, rowLen, colLen, row, col);
-
-    }
-    public void setMatrixToZero(int[][] matrix, int rowLen, int colLen, int[] row, int[] col)
-    {
-        for(int i=0;i<rowLen;i++){
-            if(row[i]==1){
-                for(int j=0;j<colLen;j++){
-                    matrix[i][j] = 0;
+                    row.add(i);
+                    col.add(j);
                 }
             }
         }
 
-        for(int i=0;i<colLen;i++){
-            if(col[i]==1){
-                for(int j=0;j<rowLen;j++){
-                    matrix[j][i] = 0;
-                }
+        for(Integer j : row){
+            for(int i=0;i<matrix[0].length;i++){
+                matrix[j][i] = 0;
             }
+
         }
+
+        for(Integer j : col){
+            for(int i=0;i<matrix.length;i++){
+                matrix[i][j] = 0;
+            }
+
+        } 
     }
 }
