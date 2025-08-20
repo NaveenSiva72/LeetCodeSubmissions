6979873ad7be9a962e4074fb5126c053fd243673
NAVEEN S <naveensiva72@gmail.com>
2025-08-21 00:43:06 +0530

    Time: 0 ms (100.00%) | Memory: 57.5 MB (37.14%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,18 +1,32 @@
 class Solution {
-    public void rotate(int[] arr, int k) {
-        int length = arr.length;
-        int starting = k%length;
-        int[] temp = new int[length - starting];
-        for(int i=0; i<(length - starting);i++){
-            temp[i] = arr[i];
+    public void rotate(int[] nums, int k) {
+        // int length = arr.length;
+        // int starting = k%length;
+        // int[] temp = new int[length - starting];
+        // for(int i=0; i<(length - starting);i++){
+        //     temp[i] = arr[i];
+        // }
+        // int j=length - starting;
+        // for(int i=0;i<starting;i++){
+        //     arr[i] = arr[j++];
+        // }
+        // j=starting;
+        // for(int i=0;i<temp.length;i++){
+        //     arr[j++]= temp[i];
+        // }  
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
-        int j=length - starting;
-        for(int i=0;i<starting;i++){
-            arr[i] = arr[j++];
-        }
-        j=starting;
-        for(int i=0;i<temp.length;i++){
-            arr[j++]= temp[i];
-        }  
     }
 }
