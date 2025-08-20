8f506c0bb541412974eeef98e2612c3ae0a1cf00
NAVEEN S <naveensiva72@gmail.com>
2025-08-21 00:37:30 +0530

    Time: 2 ms (25.47%) | Memory: 57.8 MB (14.30%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -3,24 +3,16 @@ class Solution {
         int length = arr.length;
         int starting = k%length;
         int[] temp = new int[length - starting];
-        // int j=0;
         for(int i=0; i<(length - starting);i++){
             temp[i] = arr[i];
         }
         int j=length - starting;
-        System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(temp));
         for(int i=0;i<starting;i++){
             arr[i] = arr[j++];
         }
-        System.out.println(Arrays.toString(arr));
         j=starting;
         for(int i=0;i<temp.length;i++){
             arr[j++]= temp[i];
-        }
-
-        
-
-        
+        }  
     }
 }
