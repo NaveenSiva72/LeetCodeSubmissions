074df68c1e59a0fb71cfb1bd0e52325baac61f8f
NAVEEN S <naveensiva72@gmail.com>
2025-08-22 23:26:43 +0530

    Time: 2 ms (81.45%) | Memory: 41.7 MB (91.01%) - LeetSync

--- 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.java
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.java
@@ -1,14 +1,13 @@
 class Solution {
     public boolean areOccurrencesEqual(String s) {
         int[] freq = new int[26];
-        int max = 0;
+        int eleFre = 0;
         for(int i=0;i<s.length();i++){
             freq[s.charAt(i)-'a']++;
-            max = Math.max(max, freq[s.charAt(i)-'a']);
         }
-        
+        eleFre = freq[s.charAt(0) - 'a'];
         for(int i=0;i<freq.length;i++){
-            if(freq[i]!=0 && freq[i]!=max){
+            if(freq[i]!=0 && freq[i]!=eleFre){
                 return false;
             }
         }
