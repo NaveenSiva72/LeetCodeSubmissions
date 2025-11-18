3d80ec44e7b8a5a71821a96bf4cecad396f218df
NAVEEN S <naveensiva72@gmail.com>
2025-11-18 22:45:55 +0530

    Time: 12 ms (29.86%) | Memory: 44.2 MB (63.41%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -5,15 +5,14 @@ class Solution {
             return false;
         }
         int[] sA = new int[26];
-        int[] tA = new int[26];
 
         for(int i=0;i<s.length();i++){
             sA[s.charAt(i) - 'a'] = ++sA[s.charAt(i) - 'a'];
-            tA[t.charAt(i) - 'a'] = ++tA[t.charAt(i) - 'a'];  
+            sA[t.charAt(i) - 'a'] = --sA[t.charAt(i) - 'a'];  
         }
 
         for(int i=0;i<26;i++){
-            if(sA[i]!=tA[i]){
+            if(sA[i]!=0){
                 return false;
             }
         }
