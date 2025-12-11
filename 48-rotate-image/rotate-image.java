689ed8401aafe475303520c283cc9f6a99d5225c
NAVEEN S <naveensiva72@gmail.com>
2025-12-11 22:47:59 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (26.03%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        for(int i=0;i<matrix.length;i++){
+            for(int j=i;j<matrix[i].length;j++)
+            {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+            // System.out.println(Arrays.deepToString(matrix));
+
+        }
+        // System.out.println(Arrays.deepToString(matrix));
+
+        for(int i=0;i<matrix[0].length;i++){
+            int st=0;
+            int end=matrix[0].length - 1;
+            while(st < end){
+                int temp = matrix[i][st];
+                matrix[i][st] = matrix[i][end];
+                matrix[i][end] = temp;
+                st++;
+                end--;
+            }
+        }
+
+
+    }
+}
