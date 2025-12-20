e24a3740f36121c69e47207b9a87afb7b845d9f8
NAVEEN S <naveensiva72@gmail.com>
2025-12-20 13:50:50 +0530

    Time: 31 ms (64.44%) | Memory: 96.2 MB (7.69%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,32 +1,72 @@
+// class Solution {
+//     public int longestConsecutive(int[] nums) {
+//         if(nums.length == 0)
+//         {
+//             return 0;
+//         }
+//         HashSet<Integer> set = new HashSet<>();
+//         HashSet<Integer> remo = new HashSet<>();
+//         int ans=1;
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             set.add(nums[i]);
+//         }
+//         while(set.size()!=0)
+//         {
+//             Iterator it = set.iterator();
+//             int curr =(int) it.next();
+//             if(!(set.contains(curr - 1)) && !(remo.contains(curr - 1)))
+//             {
+//                ans=Math.max(ans , chkInTheDecreasingOrder(set, remo , curr));
+//             }else{
+//                 remo.add(curr);
+//                 set.remove(curr);
+//             }
+//             // System.out.println(curr +"curr");
+//             // System.out.println(remo +"remo");
+//             // System.out.println(set +"set");
+//             // System.out.println(ans +"ans");
+//         }
+//         return ans;
+//     }
+//     public int chkInTheDecreasingOrder(HashSet<Integer> set, HashSet<Integer> remo , int currentNum)
+//     {
+//         int inc=0;
+//         // currentNum;
+//         while(set.remove(currentNum) || remo.remove(currentNum))
+//         {
+//             inc++;
+//             currentNum++;
+//         }
+
+//         return inc;
+//     }
+// }
+
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if(nums.length == 0)
-        {
-            return 0;
-        }
-        HashSet<Integer> set = new HashSet<>();
-        int ans=1;
-        for(int i=0;i<nums.length;i++)
-        {
-            set.add(nums[i]);
-        }
+        HashSet<Integer> map=new HashSet<>();
         for(int i : nums)
         {
-            if(!(set.contains(i - 1)))
+            map.add(i);
+        }
+        int curr=1;
+        int next=0;
+        int ans=0;
+        for(Integer i : map)
+        {
+            if(!(map.contains(i-1)))
             {
-               ans=Math.max(ans , chkInTheDecreasingOrder(set , i));
+                next=i+1;
+                curr=1;
+                while(map.contains(next))
+                {
+                    curr++;
+                    next=next+1;
+                }
+                ans=Math.max(curr , ans);
             }
         }
         return ans;
-    }
-    public int chkInTheDecreasingOrder(HashSet<Integer> set , int currentNum)
-    {
-        int inc=1;
-        while(set.contains(++currentNum))
-        {
-            inc++;
-        }
-
-        return inc;
     }
 }
