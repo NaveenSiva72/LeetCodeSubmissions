5b61336f886b1b8b1ac24f6ec3595ca0ccde38b6
NAVEEN S <naveensiva72@gmail.com>
2026-07-29 23:26:41 +0530

    Time: 10 ms (28.69%) | Memory: 44.1 MB (90.04%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,23 +1,23 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-
         if(s.length() != t.length()){
             return false;
         }
-        int[] sA = new int[26];
+        int[] fi = new int[26];
+        int[] se = new int[26];
 
-        for(int i=0;i<s.length();i++){
-            sA[s.charAt(i) - 'a'] = ++sA[s.charAt(i) - 'a'];
-            sA[t.charAt(i) - 'a'] = --sA[t.charAt(i) - 'a'];  
+        for(int i=0;i<s.length() && i<t.length();i++){
+            fi[s.charAt(i)-'a'] = ++fi[s.charAt(i)-'a'];
+            se[t.charAt(i)-'a'] = ++se[t.charAt(i)-'a'];
         }
 
         for(int i=0;i<26;i++){
-            if(sA[i]!=0){
+            if(fi[i]!=se[i]){
                 return false;
             }
         }
-
         return true;
+        
         
     }
 }
