93d34585d7bce31d9d2aa6f8f5cf409881055638
NAVEEN S <naveensiva72@gmail.com>
2026-08-06 22:35:24 +0530

    Time: 12 ms (24.89%) | Memory: 50.6 MB (11.29%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,22 +1,36 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String , List<String>> map= new HashMap<>();
-        for(int i=0;i<strs.length;i++)
-        {
-            String temp=strs[i];
-            char[] arr=temp.toCharArray();;
-            Arrays.sort(arr);
-            String s=new String(arr);
-            if(map.containsKey(s))
-            {
-                map.get(s).add(strs[i]);
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        StringBuilder curr = new StringBuilder("");
+        int[] cou = new int[26];
+        for(int i=0;i<strs.length;i++){
+            for(int j=0;j<strs[i].length();j++){
+                cou[strs[i].charAt(j) - 'a'] = cou[strs[i].charAt(j) - 'a'] + 1;   
             }
-            else
-            {
-                map.put(s , new ArrayList<>());
-                map.get(s).add(strs[i]);
+            for(int j=0;j<26;j++){
+                int a = (int)'a';
+                for(int k=cou[j];k>0;k--){
+                    Character b = (char)(a + j);
+                    curr.append(b);
+                    cou[j] = cou[j] - 1;
+                }
             }
+            String curr2= curr.toString();
+            if(map.containsKey((curr2))){
+                map.get(curr2).add(strs[i]);
+                map.put(curr2, map.get(curr2));
+            }else{
+                List<String> sans = new ArrayList<>();
+                sans.add(strs[i]);
+                map.put(curr2, sans);
+            }
+            curr.setLength(0);
         }
-        return new ArrayList<>(map.values());
+        List<List<String>> ans = new ArrayList<>();
+        for(String i : map.keySet()){
+            ans.add(map.get(i));
+        }
+        return ans;
     }
 }
